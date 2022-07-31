ea34f72af3ea41d1720a8998543774c6dd97097b
Rodan Ramdam <rodan.ramdam@gmail.com>
2022-07-31 18:31:10 +0545

    feat: simple demo of receiving requests from the user for the requested service provider

--- apps/sewasubidha-provider/src/App.js
+++ apps/sewasubidha-provider/src/App.js
@@ -1,7 +1,44 @@
 import "./App.css";
-
+import io from "socket.io-client";
+import { useEffect, useState } from "react";
+const socket = io.connect("http://localhost:3001");
 function App() {
-  return <h1>Provider App</h1>;
+  const [incomingService, setIncomingService] = useState([]);
+  const [newService, setNewService] = useState({});
+  useEffect(() => {
+    if (Object.keys(newService).length !== 0) {
+      setIncomingService((oldValues) => [newService, ...oldValues]);
+      setNewService({});
+    }
+  }, [newService]);
+  //hardcoded service prvider Id
+  socket.on("62e1300b4b8dcb57d42f8a2f", (payload) => {
+    // setIncomingService((prevState) => {
+    //   return [...prevState, payload];
+    // });
+    // setIncomingService((oldValues) => [payload, ...oldValues]);
+    setNewService(payload);
+  });
+  console.log(incomingService);
+  return (
+    <>
+      {incomingService.length > 0 ? (
+        <div>
+          {incomingService.map((service, index) => {
+            return (
+              <div key={index}>
+                <h3>ServiceName: {service.serviceName}</h3>
+                <p>UserId: {service.userId}</p>
+                <p>ServiceId: {service.serviceId}</p>
+              </div>
+            );
+          })}
+        </div>
+      ) : (
+        <></>
+      )}
+    </>
+  );
 }
 
 export default App;
