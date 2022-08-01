e120d8bd8639c47880a2d0d5d231e1d7b1d17d83
Rodan Ramdam <rodan.ramdam@gmail.com>
2022-08-01 09:32:54 +0545

    feat: add harcoded db immitating a logged in provider

---
+++ apps/sewasubidha-provider/src/db.js
@@ -0,0 +1,7 @@
+export const db = {
+  serviceProviderId: "62e1300b4b8dcb57d42f8a2f",
+  acessToken:
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyZTEzMDBiNGI4ZGNiNTdkNDJmOGEyZiIsInJvbGUiOiJzZXJ2aWNlUHJvdmlkZXIiLCJpYXQiOjE2NTkzMjMxNDQsImV4cCI6MTY1OTQwOTU0NH0.EKxhpA9OH0kNtf3z_Mq1XTIbzkIkFain9eqxX2ySnu0",
+  fullName: "ServiceProvider",
+  mobile: "spMobile",
+};
